5236114b3b84f8f13347d67712626ba6bbbb1bd0
kkchan912 <chankokkiat912@gmail.com>
2025-10-11 18:22:35 +0800

    更新 example.js

--- provider/example.js
+++ provider/example.js
@@ -1,8 +1,8 @@
 // Provider 配置示例
 // 请根据你的实际情况修改此文件
 module.exports = {
-  type: 'clash', // 订阅类型：支持 shadowsocks, v2ray, trojan, https, clash 等
-  url: 'https://raw.githubusercontent.com/aiboboxx/clashfree/main/clash.yml', // 你的订阅链接
+  type: 'shadowsocks', // 订阅类型：支持 shadowsocks, v2ray, trojan, https, clash 等
+  url: 'ss://YWVzLTI1Ni1nY206S0pGMjkxU1NtSnU5T2llYkhxWGNWdmlyMVBaNWFaRE56VFdqenN6WE0vcz0=@tw.kkchan.eu.org:10041#shadowsocks-10041', // 你的订阅链接
   udpRelay: true, // 启用 UDP 转发
   addFlag: true,   // 添加国旗图标
   tls13: true,     // 启用 TLS 1.3
